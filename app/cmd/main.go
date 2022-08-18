bfa5bec20d0715aae68c4c94a71d42bdc1bfe71c
Satoshi <satoshi.sugai.g@gmail.com>
2022-08-18 19:39:09 +0900

    update

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
 
+	pb "github.com/satttto/bookshelf/proto/api"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	server := grpc.NewServer()
+
+	pb.RegisterGreetingServiceServer(server)
+
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		server.Serve(listener)
+	}()
+
+	// Shutdown with Ctrl + C
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	server.GracefulStop()
 }
